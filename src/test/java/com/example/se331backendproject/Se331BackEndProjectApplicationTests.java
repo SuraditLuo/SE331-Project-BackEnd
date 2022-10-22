50cc2b224b8507d2ff32e89821d0fe2a6a90aedd
Suradit Luo <suradit_luo@cmu.ac.th>
2022-10-22 13:53:08 +0700

    Initialize Project

---
+++ src/test/java/com/example/se331backendproject/Se331BackEndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.se331backendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se331BackEndProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
