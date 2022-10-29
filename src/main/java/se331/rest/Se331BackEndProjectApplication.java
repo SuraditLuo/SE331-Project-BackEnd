926681d72ef8d20f3aff6a5c3e2f0aeb9eca2b91
nantipak tathong <auto0032@icloud.com>
2022-10-29 12:01:46 +0700

    fix param

--- src/main/java/se331/rest/Se331BackEndProjectApplication.java
+++ src/main/java/se331/rest/Se331BackEndProjectApplication.java
@@ -19,7 +19,7 @@ public class Se331BackEndProjectApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","http://44.210.214.188:8001/")
+                        .allowedOrigins("http://localhost:3000","http://44.210.214.188:8001")
                         .exposedHeaders("x-total-count");
 
             }
